096002c603ad5b949c37c5965aa3902aa088da9b
zackynz <nurasfanz@gmail.com>
2024-04-05 16:02:48 +0800

    done

--- ActivitySearch.cpp
+++ ActivitySearch.cpp
@@ -71,5 +71,15 @@ void LinearSearch()
 
 void display()
 {
-    
+    cout << "Array elements are: ";
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    LinearSearch();
+    display();
 }
