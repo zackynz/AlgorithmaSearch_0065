f79dccce777c7c85a9f70bb28685b250e41ac0c6
zackynz <nurasfanz@gmail.com>
2024-04-05 15:26:27 +0800

    masukkan fungsi for

--- ActivitySearch.cpp
+++ ActivitySearch.cpp
@@ -21,6 +21,10 @@ void input()
     cout << "\n---------------------\n";
     cout << " Enter array elements \n";
     cout << "----------------------\n";
+    for (i = 0; i < n; i++)
+    {
+        
+    }
 
 
 }
