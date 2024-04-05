73c961deacf7338122172498d403f60c84df3ef4
zackynz <nurasfanz@gmail.com>
2024-04-05 15:47:48 +0800

    item ditemukan di posisi (i + 1)" di layar, diikuti oleh baris baru

--- ActivitySearch.cpp
+++ ActivitySearch.cpp
@@ -46,7 +46,13 @@ void LinearSearch()
         ctr = 0;
         i = 0; //step 2
         while (i < n) // step 3
-        {}
+        {
+            ctr++;
+            if(arr[i] == item)
+            {
+                cout << "\n" << item << " found at position " << (i + 1) << endl; // step 6
+            }
+        }
     }
 
 }
