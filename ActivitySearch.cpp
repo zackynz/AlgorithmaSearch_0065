e292a53ab3ee167a1d70134730ab2ba234ff0df0
zackynz <nurasfanz@gmail.com>
2024-04-05 15:21:27 +0800

    masukkan fungsi else dan maximum element array

--- ActivitySearch.cpp
+++ ActivitySearch.cpp
@@ -12,5 +12,7 @@ void input() {
         cin >> n;
         if ((n > 0) && (n <= 20))
             break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
     }
 }
